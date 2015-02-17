3f46eab016c315f297f0b7bed7114cc268dccf8e
André Alçada Padez <andre.padez@gmail.com>
2015-02-17 17:14:12 +0100

    added configuration for compiling other modules

--- config/grunt/copy.js
+++ config/grunt/copy.js
@@ -1,3 +1,11 @@
+var fs = require('fs');
+var path = require('path');
+var grunt = require('grunt');
+var globals = require('../globals');
+var pkg = grunt.file.readJSON('./package.json');
+
+var modulesViewConfig = { files: [] };
+
 module.exports = {
     html: {
         files: [
@@ -32,5 +40,21 @@ module.exports = {
                 dest: '<%= config.target.dev %>'
             }
         ]
-    }
+    }, 
+    modulesViews: modulesViewConfig
 };
+
+(function createModulesViewsCopyTask(){
+    var modules = Object.keys(pkg.devDependencies).forEach(function(dependency){
+        if(dependency.indexOf('oet') === -1){ return; };
+
+        var viewsPath = path.join('node_modules', dependency, 'src', 'views');
+        if( !fs.existsSync(viewsPath) ) { return; }
+        modulesViewConfig.files.push({
+            expand: true,
+            cwd: viewsPath,
+            src: ['**/*.html'],
+            dest: 'dist/dev/views/'
+        });
+    });
+})();
--- config/grunt/sass.js
+++ config/grunt/sass.js
@@ -1,3 +1,9 @@
+var fs = require('fs');
+var path = require('path');
+var grunt = require('grunt');
+var globals = require('../globals');
+var pkg = grunt.file.readJSON('./package.json');
+
 module.exports = {
     // development mode
     dev: {
@@ -9,3 +15,15 @@ module.exports = {
         }
     }
 };
+
+(function writeScssDependencies(){
+    var includesSCSS = ['@charset "UTF-8";', '@import "../node_modules/oet-editor-styles/css/layout.scss";'];
+    var modules = Object.keys(pkg.devDependencies).forEach(function(dependency){
+        if(dependency.indexOf('oet') === -1){ return; };
+        var scssPath = path.join('node_modules', dependency, 'src', 'css', 'main.scss');
+        if( !fs.existsSync(scssPath) ) { return; }
+        includesSCSS.push( '@import "'+scssPath+'";' );
+    });
+
+    fs.writeFileSync(globals.config.project.root+'layout.scss', includesSCSS.join('\n'), {encoding: 'utf8'});
+})();
